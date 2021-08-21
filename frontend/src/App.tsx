79b3f290d10d73d2d0e2593ea5b52597ae72d3ae
RYUSEULGI <71699667+RYUSEULGI@users.noreply.github.com>
2021-08-21 18:35:03 +0900

    feat:route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route } from 'react-router';
 import { LoginApp } from '@/webapp/member/index';
+import { TodoListApp } from './webapp/todoList/index';
 
 const App = () => {
     return (
         <>
             <Route exact path={'/user/login'} component={LoginApp}></Route>
+            <Route exact path={'/todo'} component={TodoListApp}></Route>
         </>
     );
 };
