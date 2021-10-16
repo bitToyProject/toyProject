4a4cd2e5753365efabc36401230cb1f75ec1b218
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-10-16 15:03:11 +0900

    merge : forMerge

--- frontend/src/webapp/common/CCstyle/CCstyle.ts
+++ frontend/src/webapp/common/CCstyle/CCstyle.ts
@@ -1,1 +1,3 @@
 export const COLOR_BLACK = "hsl(0, 0%, 0%)";
+export const COLOR_WHITE = "hsl(1, 100%, 100%)";
+export const DEFAULT_BACKGROUND = "hsl(0, 0%, 20%)";
--- frontend/src/webapp/components/login/LoginPage.tsx
+++ frontend/src/webapp/components/login/LoginPage.tsx
@@ -8,6 +8,12 @@ import {
   loginState,
 } from "@/webapp/recoil/login/login";
 import { ColoredButton } from "@/webapp/container";
+import { css } from "@emotion/react";
+import {
+  COLOR_WHITE,
+  DEFAULT_BACKGROUND,
+} from "@/webapp/common/CCstyle/CCstyle";
+/** @jsxImportSource @emotion/react */
 
 interface loginValType {
   email: string;
@@ -20,8 +26,8 @@ const LoginPage = () => {
   });
   const [disabled, setDisabled] = useState<boolean>(false);
   const [loginInfo, setLoginInfo] = useRecoilState<ILoginType>(loginState);
-  const apiLoginCall = useRecoilValue(apiLogin(loginInfo));
-  console.log(apiLoginCall);
+  // const apiLoginCall = useRecoilValue(apiLogin(loginInfo));
+  // console.log(apiLoginCall);
   const handleClick = (
     e: MouseEvent<HTMLButtonElement | HTMLAnchorElement>
   ) => {
@@ -33,32 +39,81 @@ const LoginPage = () => {
   console.log("loginVal", loginVal);
   return (
     <>
-      <label>이메일</label>
-      <InputModule
-        type={"text"}
-        disabled={false}
-        placeholder={"Email"}
-        onChange={(value: string) => setLoginVal({ ...loginVal, email: value })}
-      />
-      <label>비밀번호</label>
-      <InputModule
-        type={"password"}
-        disabled={false}
-        placeholder={"Password"}
-        onChange={(value: string) =>
-          setLoginVal({ ...loginVal, password: value })
-        }
-      />
-      <ColoredButton
-        disabled={disabled}
-        btnLabel={"로그인하기"}
-        color={""}
-        backgroundColor={""}
-        isWhite
-        handleClick={handleClick}
-      />
+      <div css={cssWrapper}>
+        <div className="inline-block-box">
+          <label>이메일</label>
+          <div>
+            <InputModule
+              type={"text"}
+              disabled={false}
+              placeholder={"Email"}
+              onChange={(value: string) =>
+                setLoginVal({ ...loginVal, email: value })
+              }
+            />
+          </div>
+          <label>비밀번호</label>
+          <div>
+            <InputModule
+              type={"password"}
+              disabled={false}
+              placeholder={"Password"}
+              onChange={(value: string) =>
+                setLoginVal({ ...loginVal, password: value })
+              }
+            />
+          </div>
+          <div className="btn-box">
+            <ColoredButton
+              disabled={disabled}
+              btnLabel={"로그인하기"}
+              color={""}
+              backgroundColor={""}
+              isWhite
+              handleClick={handleClick}
+            />
+          </div>
+        </div>
+        <div
+          css={css`
+            width: 10px;
+            height: 10px;
+          `}
+        ></div>
+      </div>
     </>
   );
 };
 
 export default LoginPage;
+const cssWrapper = css`
+  width: 85%;
+  border: 0.5rem solid ${COLOR_WHITE};
+  min-width: 600px;
+  max-width: 1650px;
+  min-height: 50vh;
+  max-height: 65vh;
+  margin: 2rem auto;
+  .inline-block-box {
+    width: 50%;
+    height: 200px;
+    margin: 0 auto;
+    input {
+      max-width: 100%;
+      min-width: 100%;
+      min-height: 5vh;
+      max-height: 5vh;
+      box-sizing: border-box;
+      border: 0.3rem solid ${COLOR_WHITE};
+      border-radius: 1px;
+      background-color: ${DEFAULT_BACKGROUND};
+    }
+    .btn-box {
+      margin-top: 3rem;
+      display: flex;
+      justify-content: end;
+      box-sizing: border-box;
+      border: 1px;
+    }
+  }
+`;
