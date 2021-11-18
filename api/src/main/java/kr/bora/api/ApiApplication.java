469181388b31400ad3ae3a2b0492a65b9e2bddf6
장해솔 <hsjang@iparking.co.kr>
2021-11-18 18:26:42 +0900

    history추적용 data envers
    Entity가 변경된 이력을 관리하는 history 기능
    사용법 : repository에 ,RevisionRepository를 상속하면 된다
    
    ```
    @NoRepositoryBean
    public interface RevisionRepository<T, ID, N extends Number & Comparable> extends Repository<T, ID> {
    Optional<Revision<N, T>> findLastChangeRevision(ID id);
    
    Revisions<N, T> findRevisions(ID id);
    
    Page<Revision<N, T>> findRevisions(ID id, Pageable pageable);
    
    Optional<Revision<N, T>> findRevision(ID id, N revisionNumber);
    
    }
    ```

--- api/src/main/java/kr/bora/api/ApiApplication.java
+++ api/src/main/java/kr/bora/api/ApiApplication.java
@@ -3,8 +3,10 @@ package kr.bora.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaRepositories
 @SpringBootApplication
 @EnableJpaAuditing
 public class ApiApplication {
