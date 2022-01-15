b550e961a009837fcad0a83a771ba5f6c6955368
장해솔 <hsjang@iparking.co.kr>
2022-01-15 14:43:41 +0900

    history 생성했던 것 제거

--- api/src/main/java/kr/bora/api/user/repository/UserHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.bora.api.user.repository;
-
-import kr.bora.api.user.domain.UserHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserHistoryRepository extends JpaRepository<UserHistory,Long> {
-}
