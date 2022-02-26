d7ac03e164e50c83a078b634fa30fb8102bc649c
haesol Jang <wkdgothf@gmail.com>
2022-02-26 15:53:32 +0900

    메일인증 완료

--- api/src/main/java/kr/bora/api/mailauth/domain/dto/AuthMailDto.java
+++ api/src/main/java/kr/bora/api/mailauth/domain/dto/AuthMailDto.java
@@ -5,8 +5,10 @@ import kr.bora.api.mailauth.domain.entity.AuthMail.AuthStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class AuthMailDto {
