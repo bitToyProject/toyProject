5ebe42e033ee545dc265b37de30b17ed7a14c2d9
VvsJaemin <djd453216@nate.com>
2022-08-06 22:25:48 +0900

    feat : 메서드 추가

--- api/src/main/java/kr/bora/api/files/repository/FileRepository.java
+++ api/src/main/java/kr/bora/api/files/repository/FileRepository.java
@@ -1,6 +1,5 @@
 package kr.bora.api.files.repository;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import kr.bora.api.files.domain.FileType;
 import kr.bora.api.files.domain.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,24 +23,27 @@ public interface FileRepository extends JpaRepository<Files, Long> {
     List<Files> findByTextFileId(@Param("fileType") FileType fileType, @Param("textId") Long textId);
 
     @Query("SELECT f from Files f where f.fileId=:fileId and f.fileType ='LOCAL' and f.deleteYn ='N'")
-    List<Files> findByLocalFileId(@Param("fileId") Long fileId);
+    List<Files> findByFileId(@Param("fileId") Long fileId);
 
-
-    @Modifying(clearAutomatically = true)
-    @Query("DELETE FROM Files f where f.fileId in (:fileId) and f.fileType=:fileType and f.todoId =:todoId")
-    void todoFilesDelete(@Param("fileId") Long[] fileId, @Param("fileType") FileType fileType, @Param("todoId") Long todoId);
+//    @Modifying(clearAutomatically = true)
+//    @Query("DELETE FROM Files f where f.fileId in (:fileId) and f.fileType=:fileType and f.todoId =:todoId")
+//    void todoFilesDelete(@Param("fileId") Long[] fileId, @Param("fileType") FileType fileType, @Param("todoId") Long todoId);
 
     @Modifying(clearAutomatically = true)
     @Query("DELETE FROM Files f where f.fileType='TEXT_EDITOR'and f.textId =:textId") // 텍스터 에디터의 게시글이 사라지면 완전삭제
     void textFilesDelete(@Param("textId") Long textId);
 
     @Modifying(clearAutomatically = true)
-    @Query("DELETE FROM Files f where f.fileType='LOCAL' AND f.fileId IN (:fileId)")
+    @Query("DELETE FROM Files f where f.fileType='TODO'and f.todoId =:todoId") // Todo 게시글이 사라지면 완전삭제
+    void todoFilesDelete(@Param("todoId") Long todoId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM Files f where f.fileType='LOCAL'and f.fileId IN (:fileId)")
     void localFilesDelete(@Param("fileId") Long fileId);
 
     @Modifying
     @Transactional
-    @Query("UPDATE Files f SET f.deleteYn = 'Y', f.modDate=current_timestamp where f.fileId IN (:fileId) and f.fileType=:fileType and f.todoId =:todoId")
+    @Query("UPDATE Files f SET f.deleteYn = 'Y' where f.fileId IN (:fileId) and f.fileType=:fileType and f.todoId =:todoId")
     void todoFilesUpdate(@Param("fileId") Long fileId, @Param("fileType") FileType fileType, @Param("todoId") Long todoId);
 
     @Modifying
@@ -51,12 +53,15 @@ public interface FileRepository extends JpaRepository<Files, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Files f SET f.deleteYn='Y', f.modDate=current_timestamp where f.fileId IN(:fileId) and f.fileType=:fileType")
-    void localFileUpdate(@Param("fileId") Long fileId, @Param("fileType") FileType fileType);
+    @Query("UPDATE Files f SET f.deleteYn = 'Y', f.modDate=current_timestamp where f.fileId IN (:fileId) and f.fileType=:fileType")
+    void localFilesUpdate(@Param("fileId") Long fileId, @Param("fileType") FileType fileType);
+
 
     @Query("SELECT f.filename from Files f where f.fileId=:fileId and f.fileType=:fileType and f.deleteYn='N'")
     String findByFileIdAndFileType(@Param("fileId") Long fileId, @Param("fileType") FileType fileType);
 
-    @Query("SELECT f.user.nickName from Files f where f.fileId IN(:fileId) and f.fileType='LOCAL'")
+    @Query("select f.user.nickName from Files f where f.fileId IN(:fileId) and f.fileType='LOCAL'")
     String getLocalFileUploader(@Param("fileId") Long fileId);
+
+
 }
