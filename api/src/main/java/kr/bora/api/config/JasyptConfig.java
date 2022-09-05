59aa5cedb0a687ab8f6cd23a255fc08d29c18494
VvsJaemin <djd453216@nate.com>
2022-09-06 00:02:48 +0900

    feat : Jasypt secretkey 환경변수에 등록

--- api/src/main/java/kr/bora/api/config/JasyptConfig.java
+++ api/src/main/java/kr/bora/api/config/JasyptConfig.java
@@ -3,24 +3,27 @@ package kr.bora.api.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
 
-        String key = "bora_jasypt_key";
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(key); // 암호화할 때 사용하는 키
+        config.setPassword(encryptKey); // 암호화할 때 사용하는 키
         config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256"); // 암호화 알고리즘
         config.setKeyObtentionIterations("10000"); // 반복할 해싱 회수
         config.setPoolSize("1"); // 인스턴스 pool
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
         config.setStringOutputType("base64"); //인코딩 방식
         encryptor.setConfig(config);
         return encryptor;
